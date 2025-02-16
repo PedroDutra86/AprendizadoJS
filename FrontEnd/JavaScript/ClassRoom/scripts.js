3fec73abfd8219dd830bc31bef1e7196c6045f4e
Pedro Dutra <pedrodutra86@hotmail.com>
2025-02-16 16:38:30 -0300

    Variaveis

--- FrontEnd/JavaScript/ClassRoom/scripts.js
+++ FrontEnd/JavaScript/ClassRoom/scripts.js
@@ -1,5 +1,11 @@
-// Comentários de uma linha
-/* Comentário
-de
-múltiplas
-linha*/
+// Variáveis
+// É um espaço utilizado na Memoria RAM para armazenar valores
+
+// Declarando variavel sem valor
+let user
+
+// Declarando variavel com valor
+let email = "Pedrodutra86@hotmail.com"
+
+// Substituindo o valor da variavel
+email = "Kathleencouto@hotmail.com"
